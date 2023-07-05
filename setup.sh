fa40cd48b16cf838560296975dd2ba562476650b
chanakac91 <chanakac.91@gmail.com>
2023-07-06 01:32:06 +0530

    remove portainer from docker network

--- setup.sh
+++ setup.sh
@@ -16,7 +16,7 @@ NAME="portainer"
 # Install Portainer
 if [ ! "$(docker ps -q -f name=$NAME)" ]; then
     docker volume create portainer_data
-    docker run -d --network xray_warp -p 9000:9000 --name portainer --restart always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+    docker run -d -p 9000:9000 --name portainer --restart always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 fi
 
 NAME="xray"
